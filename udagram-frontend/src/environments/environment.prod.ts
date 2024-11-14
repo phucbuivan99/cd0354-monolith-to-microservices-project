f0a9e38faf2659878ab5b5d20e9f5903d2f2ed8d
phucbuivan99 <phucbuivan99@gmail.com>
2024-11-14 23:23:09 +0700

    update .travis.yml to build frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Udagram Microservice',
+  apiHost: 'http://ad1b417beca6543bdb4f1b653fa23914-1144746082.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
